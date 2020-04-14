5619ffef2e3e93785cc30db68ff79a7b95f9b403
Rodier <francisrodier78@yahoo.fr>
2020-04-14 18:54:07 +0200

    Expression arithmétique postfixée en arbre binaire III.

--- 5. Les données abstraites/Expression arithmétique postfixée en arbre binaire.php
+++ 5. Les données abstraites/Expression arithmétique postfixée en arbre binaire.php
@@ -54,3 +54,151 @@ class CNoeud {
 	}
 }
 
+class CArbre {
+	private $racine;
+
+	public function __construct() {
+		$this->racine = null;
+	}
+
+	public function change_racine(CNoeud $NdCourant) {
+		$this->racine = $NdCourant;
+	}
+
+	public function ParcourPrefixe(CNoeud $NdCourant, $separateur) {
+		if ($NdCourant != null) {
+			$NdCourant->Traiter($separateur);
+
+			if ($NdCourant->gauche() != null) {
+				$this->ParcoursPrefixe($NdCourant->gauche(), $separateur);
+			}
+
+			if ($NdCourant->droit() != null) {
+				$this->ParcoursPrefixe($NdCourant->droit(), $separateur);
+			}
+		}
+	}
+
+	public function ParcourInfixe(CNoeud $NdCourant, $separateur) {
+		if ($NdCourant != null) {
+			echo "(";
+
+			if ($NdCourant->gauche() != null) {
+				$this->ParcoursInfixe($NdCourant->gauche(), $separateur);
+			}
+
+			$NdCourant->Traiter($separateur);
+
+			if ($NdCourant->droit() != null) {
+				$this->ParcoursInfixe($NdCourant->droit(), $separateur);
+			}
+
+			echo ")";
+		}
+	}
+
+	public function ParcourPostfixe(CNoeud $NdCourant, $separateur) {
+		if ($NdCourant != null) {
+			if ($NdCourant->gauche() != null) {
+				$this->ParcoursPostfixe($NdCourant->gauche(), $separateur);
+			}
+
+			if ($NdCourant->droit() != null) {
+				$this->ParcoursPostfixe($NdCourant->droit(), $separateur);
+			}
+
+			$NdCourant->Traiter($separateur);
+		}
+	}
+
+	public function ParcourParNiveaux(CNoeud $NdCourant, $separateur) {
+		$UneFile = new SplQueue;
+		$UneFile->enqueue($NdCourant);
+
+		while (!$UneFile->isEmpty()) {
+			$NdCourant->Traiter($separateur);
+
+			if ($NdCourant->gauche() != null) {
+				$UneFile->enqueue($NdCourant->gauche());
+			}
+
+			if ($NdCourant->droit() != null) {
+				$UneFile->enqueue($NdCourant->droit());
+			}
+		}
+	}
+
+	public function evaluer(CNoeud $NdCourant) {
+		if ($NdCourant->get_ope() == '0') {
+			$res = $NdCourant->get_val();
+		} else {
+			$valeur1 = $this->evaluer($NdCourant->gauche());
+			$valeur2 = $this->evaluer($NdCourant->droit());
+			
+			if ($NdCourant->get_ope() == '+') {
+				$res = $valeur1 + $valeur2;
+			} else if ($NdCourant->get_ope() == '-') {
+				$res = $valeur1 - $valeur2;
+			} else if ($NdCourant->get_ope() == '*') {
+				$res = $valeur1 * $valeur2;
+			} else if ($NdCourant->get_ope() == '/') {
+				$res = $valeur1 / $valeur2;
+			}
+		}
+		return $res;
+	}
+}
+
+// === Programme ===
+$UnePile = new SplStack();
+$UnArbre = new CArbre();
+
+echo "Entrez une notation en polonaise inversée";
+$phrase = trim(fgets(STDIN));
+$nbcaract = strlen($phrase);
+$i = 0;
+
+while ($i < $nbcaract) {
+	$car = $phrase($i);
+
+	if (($car >= '0') && ($car <= '9')) {
+		$chiffre = 0;
+		$nombre = 0;
+		while (($car >= '0') && ($car <= '9')) {
+			$chiffre = ord($car) - 48;
+			$nombre = (($nombre * 10) + $chiffre);
+			$car = $phrase[++$i];
+		}
+		$NvNoeud = new CNoeud('0', $nombre);
+		$UnePile->push($NvNoeud);
+	} else if (($car == '+') || ($car == '-') || ($car == '*') || ($car == '/')) {
+		$NvNoeud = new CNoeud($car,0);
+		$NvNoeud->change_droit($UnePile->pop);
+		$NvNoeud->change_gauche($UnePile->pop);
+		$UnePile->push($NvNoeud);
+	}
+	
+	$i++;
+}
+
+$UnArbre->change_racine($UnePile->pop());
+
+echo "Parcours Préfixé : ";;
+$UnArbre->ParcoursPrefixe($UnArbre->val_racine(), ' ');
+echo PHP_EOL;
+
+echo "Parcours Infixé : ";;
+$UnArbre->ParcoursInfixe($UnArbre->val_racine(), ' ');
+echo PHP_EOL;
+
+echo "Parcours Postfixé : ";;
+$UnArbre->ParcoursPostfixe($UnArbre->val_racine(), ' ');
+echo PHP_EOL;
+
+echo "Parcours par niveaux : ";;
+$UnArbre->ParcoursParNiveaux($UnArbre->val_racine(), ' ');
+echo PHP_EOL;
+
+printf("Calcul de l'expression : %5.2f\n", $UnArbre->evaluer($UnArbre->val_racine()));
+echo PHP_EOL;
+?>
